66ccd3f674c96db06345093fca020e35b7694084
MattHandzel <matthewwalkosz@gmail.com>
2022-05-01 15:28:20 -0500

    Made it look like a bad dark mode! Also added button to delete a task, as well as reset all tasks, etc. It looks very fancy

--- script.js
+++ script.js
@@ -1,80 +1,146 @@
 
-function checkProgress() {
-    var filledOut = 0;
-    document.getElementById("progressBarParent").style.width = window.innerWidth + "px";
-    document.getElementsByName("checkbox").forEach(element => { filledOut += element.checked ? 1 : 0; });
-    var intendedSize = Math.round(filledOut / document.getElementsByName("checkbox").length * document.getElementById("progressBarParent").style.width.toString().slice(0, document.getElementById("progressBarParent").style.width.toString().length - 2));
-    intendedSize = String(intendedSize) == "NaN" ? 0 : intendedSize;
-    document.getElementById("progressBar").style.width = intendedSize + "px";
+var completedTaskColor = "lightgreen"
 
-    console.log(Math.round(filledOut / document.getElementsByName("checkbox").length * document.getElementById("progressBarParent").style.width.toString().slice(0, document.getElementById("progressBarParent").style.width.toString().length - 2)) + "px");
+function checkProgress() {
+    /*This function will update the progress bar, it works by getting the width of the max progress bar size (its parent's size) and then dividing that
+    by the amount of tasks that are completed*/
+    var filledOut = 0;
+
+    let progressBarParent = document.getElementById("progressBarParent");
+    let parents = document.getElementsByClassName("parentDiv");
+    let checks = []
+    for (i = 0; i < parents.length; i++) {
+        checks.push(parents[i].childNodes[0])
+        console.log(parents[i].childNodes[0])
+    }
+
+    let progressBar = document.getElementById("progressBar")
+
+    let totalItems = checks.length;
+
+    progressBarParent.style.width = window.innerWidth + "px";
+
+    for (i = 0; i < checks.length; i++) {
+        console.log(checks[i])
+        filledOut += checks[i].style.color === completedTaskColor ? 1 : 0;
+    }
+
+    // * This prevents it from showing NaN%
+    if (totalItems == 0) {
+        filledOut = 0;
+        totalItems = 1;
+    }
+
+    var intendedSize = Math.round(filledOut / totalItems * progressBarParent.style.width.toString().slice(0, progressBarParent.style.width.toString().length - 2));
+    intendedSize = String(intendedSize) == "NaN" ? 0 : intendedSize;
+    progressBar.style.width = intendedSize + "px";
+    progressBar.innerText = String(Math.round(100 * filledOut / totalItems)) + "%";
+
 }
 
 
 function addNewItem() {
+    /*This function will add a new item that will have the default values of being called thing to do, and having
+    its completion status set to false*/
     addItem();
 }
 
 function addItem(itemText = "Thing To Do", completionStatus = false) {
+    /* This function will add a check box as well as an input, this will be
+        where the user can put in the thing that they want to do, and when 
+        they are done with the task they can click on the checkbox
+     */
+
+    let parent = document.createElement("div");
     let new_br = document.createElement("br");
-    let new_checkbox = document.createElement("input");
+    let check = document.createElement("i");
     let new_item = document.createElement("input");
+    let new_delete = document.createElement("i");
 
-    let thisItemNumber = (document.getElementsByName("checkbox").length + 1);
-    new_checkbox.type = "checkbox";
-    new_checkbox.name = "checkbox";
-    new_checkbox.id = "item" + thisItemNumber + "Checkbox";
-    new_checkbox.onclick = checkProgress;
-    new_checkbox.checked = completionStatus == "true";
+    let toDoList = document.getElementById("toDoList");
 
+    // Initialize the checkbox
+    let thisItemNumber = (document.getElementsByClassName("parentDiv").length + 1);
+
+    parent.id = thisItemNumber + "_parentDiv";
+    parent.className = "parentDiv"
+
+    check.id = thisItemNumber + "_checkButton"
+    check.className = "material-icons"
+    check.innerHTML = "check"
+    check.style = completionStatus === "true" ? "color: green;" : "color: black;";
+
+    // Initialize the input bar
     new_item.type = "text";
     new_item.type = "text";
     new_item.name = "itemInput";
+    new_item.className = "itemInput"
     new_item.id = "item" + thisItemNumber + "Input";
     new_item.value = itemText;
 
-    new_checkbox.addEventListener("click", saveData);
+    // Initialize the delete button
+    new_delete.className = "material-icons"
+    new_delete.style = "color: red;"
+    new_delete.innerHTML = "delete"
+    new_delete.id = thisItemNumber + "_deleteButton"
+
+    // Make it so that when these things are clicked the saveData command is run
+    check.addEventListener("click", (event) => {
+        if (event.target.style.color != completedTaskColor) {
+            event.target.style.color = completedTaskColor;
+        }
+    })
+    check.addEventListener('click', checkProgress)
+    check.addEventListener("click", saveData);
     new_item.addEventListener("change", saveData);
+    new_delete.addEventListener("click", (element) => { deleteSpecificItemByNumber(String(element.srcElement.id.split("_")[0])) });
+    new_delete.addEventListener("click", saveData)
+    // Add the items ot the toDoList div
 
-    document.getElementById("toDoList").appendChild(new_checkbox);
-    document.getElementById("toDoList").appendChild(new_item);
-    document.getElementById("toDoList").appendChild(new_br);
-
+    parent.appendChild(check);
+    parent.appendChild(new_item);
+    parent.appendChild(new_delete);
+    parent.appendChild(new_br);
+    toDoList.appendChild(parent);
+    // Save
     saveData();
 }
 
+
 function loadLocalStorageData() {
+
     if (localStorage.toDoListData) {
         // If todolist data exists
         let splitText = localStorage.toDoListData.split("\n")
         splitText = splitText.slice(0, splitText.length - 1)
+
+        // ! If the user puts in a pipe "|" then this could be "hacked"
+        // Reads the data from the splitText thing
         for (let line in splitText) {
             let arr = splitText[line].split("|")
-            console.log(arr[3])
             addItem(arr[0], arr[1])
         }
     }
     else {
-        // If it doesn't exist
+        // Initialize the toDoListData object
+        //? This might be redundant
         localStorage.toDoListData = "";
     }
-
 
 }
 
 function saveData() {
+    // Function that stores the data into localStorage
+    /*
+        || It might be more efficient to use sessionStorage and then once the tab is closed or something
+    */
     localStorage.toDoListData = "";
-    console.log("SAVING DATA");
-    let i = 1;
-    document.getElementsByName("itemInput").forEach(item => {
-        thisItemNumber = i++;
-        console.log(i)
+    var parents = document.getElementsByClassName("parentDiv")
+    for (i = 0; i < parents.length; i++) {
+        //thisItemNumber = Number(parents[i].id.split("_")[0])
+        localStorage.toDoListData += parents[i].childNodes[1].value + "|" + String(parents[i].childNodes[0].style.color === completedTaskColor) + "\n";
+    }
 
-        checkboxStatus = document.getElementById("item" + thisItemNumber + "Checkbox")
-        console.log(checkboxStatus)
-        localStorage.toDoListData += item.value + "|" + String(checkboxStatus.checked) + "\n";
-
-    });
 }
 
 function deleteAllData() {
@@ -91,25 +157,31 @@ function deleteAllData() {
 
 }
 
+function deleteSpecificItemByNumber(number) {
+    let parentDivs = document.getElementsByClassName("parentDiv")
+    for (let i in parentDivs) {
+
+        if (parentDivs[i].id.split("_")[0] == number) {
+            removeThisDiv = parentDivs[i]
+            break;
+        }
+    }
+    toDoList.removeChild(removeThisDiv)
+}
+
+
 // Functinos that should run at the start of the code, loads data
 loadLocalStorageData();
 
 
 var addNewItemButton = document.getElementById("addNewItemButton");
-addNewItemButton.addEventListener("click", checkProgress);
 addNewItemButton.addEventListener("click", addNewItem);
-
+addNewItemButton.addEventListener("click", checkProgress);
 addNewItemButton.addEventListener("click", saveData);
 
-let deleteAllDataButton = document.getElementById("deleteAllDataButton");
+let deleteAllDataButton = document.getElementById("resetButton");
 deleteAllDataButton.addEventListener("click", deleteAllData);
 deleteAllDataButton.addEventListener("click", checkProgress)
-
-/*this.addEventListener('keypress', event => {
-    if (event.code == "Enter") {
-        saveData();
-    }
-})*/
 
 document.getElementById("progressBarParent").style.width = window.innerWidth + "px";
 
@@ -117,4 +189,5 @@ window.addEventListener("resize", checkProgress);
 
 
 // Functions that need to run after the code 
+// Updates the progress bar
 checkProgress();
